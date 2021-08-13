8fa904eda89d3762a11a63c8b921fdb775df5893
HenrikJava <henriklarssonmail@gmail.com>
2021-08-13 14:08:23 +0200

    dsf

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       <div>
-        <input type="button" value="NEW" onClick={() => { onCreate(); getTodos(setTodos)} } />
+        <input type="button" value="NEWs" onClick={() => { onCreate(); getTodos(setTodos)} } />
       </div>
       <table border="1">
         <thead>
